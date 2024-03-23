d432a4652d7847f0f10939884649cad69e28323f
SveIvanov <s.g.ivanov87@gmail.com>
2024-03-23 15:13:53 +0200

    Identity Migration

--- TrainingApp/Data/Migrations/20240323131227_Identity.Designer.cs
+++ TrainingApp/Data/Migrations/20240323131227_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrainingApp.Data;
 
@@ -11,9 +12,10 @@ using TrainingApp.Data;
 namespace TrainingApp.Data.Migrations
 {
     [DbContext(typeof(TrainingDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240323131227_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

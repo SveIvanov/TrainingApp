7e671bf0dd5f25c04c21f7be5370b45b838fe781
SveIvanov <s.g.ivanov87@gmail.com>
2024-04-07 14:11:54 +0300

    Add View Models

---
+++ TrainingApp.Core/Models/CardioTraining/AllCardioTraining.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp.Core.Models.CardioTraining
+{
+	public class AllCardioTraining
+	{
+	}
+}
